18d720b63835c80c48db20b6307a509a8cff11f6
talitaaraujodev <talitacumi.araujo@gmail.com>
2022-11-16 12:15:12 -0300

    test: added test unit tipoCarteiras controller e service

--- src/app/controllers/tipoCarteira/tipoCarteira.controller.spec.ts
+++ src/app/controllers/tipoCarteira/tipoCarteira.controller.spec.ts
@@ -1,18 +1,82 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CreateTipoCarteiraDto } from './../../persistence/dto/createTipoCarteira.dto';
+import { TipoCarteira } from '../../persistence/entities/tipoCarteira.entity';
+import { TipoCarteiraService } from '../../services/tipoCarteira/tipoCarteira.service';
 import { TipoCarteiraController } from './tipoCarteira.controller';
 
-describe('TipoCarteiraController', () => {
-  let controller: TipoCarteiraController;
-
+const mockSut = () => {
+  const tiposCarteiras: TipoCarteira[] = [
+    new TipoCarteira('any_tipo1', 'any_descricao'),
+    new TipoCarteira('any_tipo2', 'any_descricao'),
+  ];
+  const newTipoCarteira = new TipoCarteira('any_tipo1', 'any_descricao');
+  return {
+    tiposCarteiras,
+    newTipoCarteira,
+  };
+};
+describe('TipoCarteiraController tests unit', () => {
+  let tipoCarteiraController: TipoCarteiraController;
+  let tipoCarteiraService: TipoCarteiraService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TipoCarteiraController],
+      providers: [
+        {
+          provide: TipoCarteiraService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(mockSut().tiposCarteiras),
+            create: jest.fn().mockResolvedValue(mockSut().newTipoCarteira),
+          },
+        },
+      ],
     }).compile();
 
-    controller = module.get<TipoCarteiraController>(TipoCarteiraController);
+    tipoCarteiraController = module.get<TipoCarteiraController>(
+      TipoCarteiraController,
+    );
+    tipoCarteiraService = module.get<TipoCarteiraService>(TipoCarteiraService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(tipoCarteiraController).toBeDefined();
+  });
+  it('should return a list of tipos carteiras successfully', async () => {
+    const { tiposCarteiras } = mockSut();
+    const result = await tipoCarteiraController.findAll();
+    expect(result).toEqual(tiposCarteiras);
+    expect(typeof result).toEqual('object');
+    expect(tipoCarteiraService.findAll).toHaveBeenCalledTimes(1);
+  });
+  it('should throw an exception when listing tipos carteiras ', () => {
+    jest
+      .spyOn(tipoCarteiraService, 'findAll')
+      .mockRejectedValueOnce(new Error());
+
+    expect(tipoCarteiraController.findAll()).rejects.toThrowError();
+  });
+
+  it('should create a tipo carteira successfully', async () => {
+    const body: CreateTipoCarteiraDto = {
+      name: 'any_tipo1',
+      descricao: 'any_descricao',
+    };
+    const result = await tipoCarteiraController.create(body);
+    const { newTipoCarteira } = mockSut();
+
+    expect(result).toEqual(newTipoCarteira);
+    expect(tipoCarteiraService.create).toHaveBeenCalledTimes(1);
+    expect(tipoCarteiraService.create).toHaveBeenCalledWith(body);
+  });
+  it('should throw an exception when create tipo carteira', () => {
+    const body: CreateTipoCarteiraDto = {
+      name: 'any_tipo1',
+      descricao: 'any_descricao',
+    };
+    jest
+      .spyOn(tipoCarteiraService, 'create')
+      .mockRejectedValueOnce(new Error());
+
+    expect(tipoCarteiraController.create(body)).rejects.toThrowError();
   });
 });
